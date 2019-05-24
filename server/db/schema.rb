1bf05e94d9fc21d767a36d1b02643ea21777c3f1
Manon Jacquin <manon.jacquin@gmail.com>
2019-05-24 08:29:56 +0100

    Build controllers

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_151841) do
+ActiveRecord::Schema.define(version: 2019_05_24_072523) do
 
   create_table "customised_memes", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_customised_memes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
