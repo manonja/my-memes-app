5c8778a9c0bd0e07d15ba2cab48dc7150b98286a
Manon Jacquin <manon.jacquin@gmail.com>
2019-05-24 10:16:59 +0100

    Create home page

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './pages/Home'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+// mdbootstrap
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
+ReactDOM.render((
+    <Router>
+        <Route path='/' render={Home} />
+</Router>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/pages/Home.js
@@ -0,0 +1,29 @@
+import React  from 'react';
+import Signin from './Signin'
+
+const Home = () => {
+    return ( 
+        <div>
+            <div className="jumbotron text-center">
+                <h2 className="card-title h2">Memes App</h2>
+                <p className="blue-text my-4 font-weight-bold">Create and collect Memes!</p>
+
+                <div className="row d-flex justify-content-center">
+                    <div className="col-xl-7 pb-2">
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga aliquid dolorem ea distinctio exercitationem delectus qui, quas eum architecto, amet quasi accusantium, fugit consequatur ducimus obcaecati numquam molestias hic itaque accusantium doloremque laudantium, totam rem aperiam.</p>
+                    </div>
+                </div>
+
+                <div className="pt-2">
+                    <button  type="button" className="btn btn-blue waves-effect">Signin</button>
+                    <button type="button" className="btn btn-outline-primary waves-effect">Signup</button>
+                </div>
+            </div>
+        </div>
+       
+    );
+} 
+        
+    
+ 
+export default Home;
