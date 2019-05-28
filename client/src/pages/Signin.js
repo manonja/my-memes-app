715cf40bd8ed5f6619759ecfd585279e54d1aab1
Thomas Edwards <thce28@gmail.com>
2019-05-28 15:56:35 +0100

    stage quotes too

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -15,7 +15,7 @@ class Signin extends Component {
         API.signin(this.state)
             .then(data => {
                 if (data.error){
-                    alert('not working!')
+                    alert("not working!")
                 }
                 else {
                     // user is authentificated!
