3cb777aa95fad83a742613db31d6b0b4c550625e
Manon Jacquin <manon.jacquin@gmail.com>
2019-05-29 10:46:11 +0100

    modal appears with fake image

--- client/src/components/Meme.js
+++ client/src/components/Meme.js
@@ -5,11 +5,10 @@ class Meme extends Component {
   
     render() { 
         const {name, url} = this.props.meme
-        return ( 
-        
+        return (   
             <div onClick={() => this.props.handleClick(this.props.meme)}>
                 <h3>{name}</h3>
-                <img className="img" src={url} alt={name}/>  
+                <img crossOrigin="Anonymous" className="img" src={url} alt={name}/>  
             </div>
          );
     }
