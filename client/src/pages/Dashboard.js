e55969d6b1aecc00b4e90784950987aec90e34ac
Manon Jacquin <manon.jacquin@gmail.com>
2019-05-30 12:10:49 +0100

    Fix dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,26 +3,25 @@ import React, { Component } from 'react';
 import CustomisedMeme from '../components/CustomisedMeme'
 import NavBar from '../components/NavBar'
 
+import API from '../API'
+
 
 class Dashboard extends Component {
-
+   
     render() { 
         const {myMemes} = this.props
-        // debugger
 
         const meme = myMemes.map(meme => (
         <CustomisedMeme 
             key={meme.id} 
             meme={meme}
-            // handleClick={this.props.handleClick}
         />))
    
         return (
             <div>
                 <NavBar/>
                 <h1>My memes</h1>
-                <div className= 'memeCollection'> {meme} </div>
-                
+                <div className= 'memeCollection'> {meme} </div>            
             </div> 
          );
     }
